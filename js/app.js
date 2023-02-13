4580e98e9b15340de00360144edd5b375508888f
Metranohm <andrewwink@gmail.com>
2023-02-13 17:46:55 -0600

    remove button

--- js/app.js
+++ js/app.js
@@ -1,14 +1,16 @@
 const btn = document.querySelector('button');
 const input = document.querySelector('input');
+const ul = document.querySelector('ul');
 
 btn.addEventListener('click', (evt)=>{
-  //create <li> element
-  //access the text from the input
-  //append the text to the <li>
-  //add the <li> (with the text included) to HTML
   const li = document.createElement('li');
-  li.textContent = input.value
+  li.textContent = input.value;
   document.querySelector('ul').appendChild(li);
+  input.value = '';
+});
 
-
-})
+ul.addEventListener('click', (evt) => {
+  if (evt.target.tagName === 'LI') {
+    ul.removeChild(evt.target);
+  }
+});
